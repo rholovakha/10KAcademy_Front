585a51e6915400c6479ae6158cdb2891fb47b4b5
andrii-maksymets <andrew.maksymets@gmail.com>
2021-05-31 22:07:07 +0300

    Added mobile view

--- src/app/modules/home/components/callback-button/callback-button.component.ts
+++ src/app/modules/home/components/callback-button/callback-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { L10N_LOCALE, L10nLocale } from 'angular-l10n';
 
 @Component({
@@ -7,6 +7,7 @@ import { L10N_LOCALE, L10nLocale } from 'angular-l10n';
   styleUrls: ['./callback-button.component.scss']
 })
 export class CallbackButtonComponent implements OnInit, OnDestroy {
+  @Input() whiteStyle: boolean;
 
   constructor(@Inject(L10N_LOCALE) public locale: L10nLocale) { }
 
